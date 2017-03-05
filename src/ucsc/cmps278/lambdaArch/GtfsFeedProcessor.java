1a3ebc716d0c1a6b2d582e5c0b686a4404229e66
Vedang Joshi <vrjoshi@ucsc.edu>
2017-03-05 14:09:59 -0800

    GTFS producer, v0.1

--- src/ucsc/cmps278/lambdaArch/GtfsFeedProcessor.java
+++ src/ucsc/cmps278/lambdaArch/GtfsFeedProcessor.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class GtfsFeedProcessor {
 
@@ -14,15 +15,26 @@ public class GtfsFeedProcessor {
 		
 		// Properties
 		Properties prop = new Properties();
+		prop.put("bootstrap.servers", "localhost:9092");
 		prop.put("acks", "all");
 		prop.put("retires", 0);
 		prop.put("batch.size", 20000);
 		prop.put("linger.ms", 1);
 		prop.put("buffer.memory", Integer.MAX_VALUE);
-		prop.put("key.serializer", "org.apache.kafka.common.serializa-tion.StringSerializer");
-		prop.put("value.serializer", "org.apache.kafka.common.serializa-tion.StringSerializer");
+		prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
 		// Producer
 		Producer<String, String> producer = new KafkaProducer<String, String>(prop); 
+		
+		for (int i = 0; i < 5; i++) {
+			producer.send(new ProducerRecord<String, String>(topic, Integer.toString(i), Integer.toString(i)));
+		}
+		
+		System.out.println("Msg sent!");
+		producer.close();
 	}
 }
+
+
+
